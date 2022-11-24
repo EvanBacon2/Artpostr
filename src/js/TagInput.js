30ec5a79600d54ab29edc074b2be1d460bf50d9e
Evan Bacon <evan@baconblvd.com>
2022-11-23 21:06:11 -0800

    updates to TagInput, pasted text will have newlines removed, fixed small sizing/alignment issues with UI

--- src/js/TagInput.js
+++ src/js/TagInput.js
@@ -5,20 +5,28 @@ import "../css/InputField.css"
 const TagInput = ({ tagKey, onSubmit }) => {
     const span = useRef()
 
-    const onS = () => {
+    const handleBlur = () => {
         onSubmit(tagKey, span.current.textContent)
     }
 
-    const onKD = (e) => {
+    const handleKeyDown = (e) => {
         if (e.key == 'Enter') {
             e.preventDefault()
-            onS()
+            handleBlur()
             span.current.blur()
         }
     }
 
+    const handlePaste = (e) => {
+        e.preventDefault()
+        let text = e.clipboardData.getData('text')
+        text = text.replaceAll(/(\r\n|\n|\r)/gm, '')
+        span.current.textContent = text
+    }
+
     return (
-        <span className="input-field tag" ref={span} contentEditable="true" onBlur={onS} onKeyDown={onKD}></span>
+        <span className="input-field tag" ref={span} contentEditable="true"
+            onBlur={handleBlur} onKeyDown={handleKeyDown} onPaste={handlePaste}></span>
     );
 }
 
